c762658c685a8d34ae6abe3f59ea415a7b83462b
Tiago-classy <tiago.ferraz@classy-advertising.com>
2024-07-10 17:03:20 +0100

    add fillna

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/open-chrome', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Redirection service running on port ${PORT}`);
 });
