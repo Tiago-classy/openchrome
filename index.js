8aad6dc94f2909d19620b8a7f1a9961d343f9515
Tiago-classy <tiago.ferraz@classy-advertising.com>
2024-07-10 18:00:05 +0100

    add fillna

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+
+
 const app = express();
+
+
+// app.get('/ninja', (req, res) => {
+//   const userAgent = req.headers['user-agent'];
+//   const targetUrl = 'https://tiago-classy.github.io/';
+
+//   if (/android/i.test(userAgent)) {
+//     res.redirect(`googlechrome://${targetUrl}`);
+//   } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+//     res.redirect(`googlechrome://${targetUrl}`);
+//   } else {
+//     res.redirect(targetUrl); // fallback to regular URL
+//   }
+// });
 
 app.get('/open-chrome', (req, res) => {
   const userAgent = req.headers['user-agent'];
-  const targetUrl = 'https://tiago-classy.github.io/';
+  const targetUrl = 'https://tiago-classy.github.io/index.html';
 
   if (/android/i.test(userAgent)) {
     res.redirect(`googlechrome://${targetUrl}`);
