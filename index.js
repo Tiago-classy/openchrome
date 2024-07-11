cb9b391d1c41ca860c70f79272694ef450334b2f
Tiago-classy <tiago.ferraz@classy-advertising.com>
2024-07-11 10:57:34 +0100

    other way

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const express = require('express');
 
-
 const app = express();
-
-
-// app.get('/ninja', (req, res) => {
-//   const userAgent = req.headers['user-agent'];
-//   const targetUrl = 'https://tiago-classy.github.io/';
-
-//   if (/android/i.test(userAgent)) {
-//     res.redirect(`googlechrome://${targetUrl}`);
-//   } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-//     res.redirect(`googlechrome://${targetUrl}`);
-//   } else {
-//     res.redirect(targetUrl); // fallback to regular URL
-//   }
-// });
 
 app.get('/open-chrome', (req, res) => {
   const userAgent = req.headers['user-agent'];
-  const targetUrl = 'tiago-classy.github.io/index.html';
-
+  const targetUrl = 'https://tiago-classy.github.io/index.html';
+  
   if (/android/i.test(userAgent)) {
-    res.redirect(`googlechrome://${targetUrl}`);
+    // Attempt to open in Chrome on Android
+    res.send(`
+      <script>
+        const url = '${targetUrl}';
+        const androidIntent = 'intent://${targetUrl.replace(/^https?:\/\//, '')}#Intent;scheme=https;package=com.android.chrome;end';
+        window.location.href = androidIntent;
+        setTimeout(() => { window.location.href = url; }, 1000); // Fallback if intent fails
+      </script>
+    `);
   } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-    res.redirect(`googlechrome://${targetUrl}`);
+    // Attempt to open in Chrome on iOS
+    res.send(`
+      <script>
+        const url = '${targetUrl}';
+        const iosChromeUrl = 'googlechrome://${targetUrl.replace(/^https?:\/\//, '')}';
+        window.location.href = iosChromeUrl;
+        setTimeout(() => { window.location.href = url; }, 1000); // Fallback if chrome URL fails
+      </script>
+    `);
   } else {
-    res.redirect(targetUrl); // fallback to regular URL
+    // Fallback to regular URL
+    res.redirect(targetUrl);
   }
 });
 
